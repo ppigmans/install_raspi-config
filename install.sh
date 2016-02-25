9b3ab99a95a813227ef898cb9d8435468a00f4ae
Patrick Pigmans <ppigmans@hotmail.com>
2016-02-25 15:55:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@
 
 cd /tmp
 wget http://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20160210_all.deb
-apt-get install libnewt0.52 whiptail parted triggerhappy lua5.1
+apt-get install libnewt0.52 whiptail parted triggerhappy lua5.1 alsa_mixer
+apt-get -f install
 dpkg -i raspi-config_20160210_all.deb
