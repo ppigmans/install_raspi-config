44c84ba073d33c0a869a7738162936f1ac287f06
Patrick Pigmans <ppigmans@hotmail.com>
2016-04-04 15:28:01 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # must run as root or use sudo
 
 cd /tmp
-wget http://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20160210_all.deb
+wget http://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20160322_all.deb
 apt-get install libnewt0.52 whiptail parted triggerhappy lua5.1 alsa-utils
 apt-get -f install #ubuntu on pi gives an error to do so so yeah :)
-dpkg -i raspi-config_20160210_all.deb
+dpkg -i raspi-config_20160322_all.deb
